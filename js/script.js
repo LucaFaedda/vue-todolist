de89e020e054e247b98ad2619074918611470df1
LucaFaedda <lucakarl91@gmail.com>
2022-12-20 20:55:32 +0100

    millstone 1 completata

--- js/script.js
+++ js/script.js
@@ -5,9 +5,47 @@ const {
 createApp({
     data(){
         return{
- 
+
+            daGuardare: [
+                {
+                    text: 'Mob 100',
+                    done: false
+                },
+                {
+                    text: 'Stranger Thinks',
+                    done: false
+                },
+                {
+                    text: 'No Time To Die',
+                    done: false
+                },
+                {
+                    text: 'Jurassic World: Domination',
+                    done: false
+                },
+                {
+                    text: 'Avatar 2',
+                    done: false
+                },
+                {
+                    text: 'Willow',
+                    done: false
+                },
+            ]
 
         }
-    }
+    },
+    methods: {
+        visto(index)
+        {
+            let fatto = this.daGuardare[index]
+            if(fatto.done == true){
+                fatto.done = false
+            }
+            else{
+                fatto.done = true
+            } 
+            }
+    },
 
 }).mount('#app')
